c04a03c6dda68bad54711fe2e952a224ba039b4f
mochiken <kengo.mochizuki@gmail.com>
2016-09-12 11:34:02 +0000

    temporarily don't use Cloudinary

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -5,11 +5,11 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    include Cloudinary::CarrierWave
-  else
+  # if Rails.env.production?
+  #   include Cloudinary::CarrierWave
+  # else
     storage :file
-  end
+  # end
 
   def public_id
     model.id
