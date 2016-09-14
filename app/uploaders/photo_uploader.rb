565d6b1acd01ebd9c5ef12d73fdc2ac3f6792145
mochiken <kengo.mochizuki@gmail.com>
2016-09-14 01:19:32 +0000

    always use Cloudinary instead of File Save

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,11 +1,12 @@
 class PhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
+  # if Rails.env.production?
+  if true                                           # always use Cloudinary
     include Cloudinary::CarrierWave
     process :convert => 'png'
     process :tags => ['mochi', "#{Time.now.strftime("%a")}", "#{Time.now.strftime("%b")}"]
